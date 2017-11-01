744218f11d53e11f1d1ebc5190c664f18b894390
Goncharov Roman <romg92@mail.ru>
2017-11-01 22:48:22 +0300

    add calc-game

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import { getUserName, gameEvenFunction } from '..';
+import { getUserName, gameCalcFunction } from '..';
 
 console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no"\n');
+console.log('What is the result of the expression?\n');
 
 const userName = getUserName();
 console.log(`Hello, ${userName}!\n`);
@@ -12,7 +12,7 @@ const iter = (acc) => {
     console.log(`Congratulations, ${userName}!`);
     return 0;
   }
-  if (gameEvenFunction(userName) === 0) {
+  if (gameCalcFunction(userName, acc) === 0) {
     return iter(acc + 1);
   }
   return 1;
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export const getUserName = () => {
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export const gameFunction = (name) => {
+export const gameEvenFunction = (name) => {
   const number = getRandom(0, 100);
   console.log(`Question: ${number}`);
   const res = readlineSync.question('Your answer: ');
@@ -27,6 +27,36 @@ export const gameFunction = (name) => {
     console.log(`Correct answer was '${res === 'yes' ? 'no' : 'yes'}'`);
   }
   console.log(`Let's try again, ${name}!`);
+  return 1;
+};
+
+export const gameCalcFunction = (name, iterNum) => {
+  const number1 = getRandom(0, 21);
+  const number2 = getRandom(0, 21);
+  if (iterNum === 0) {
+    console.log(`Question: ${number1} + ${number2}`);
+  } else if (iterNum === 1) {
+    console.log(`Question: ${number1} - ${number2}`);
+  } else if (iterNum === 2) {
+    console.log(`Question: ${number1} * ${number2}`);
+  }
+  const res = readlineSync.question('Your answer: ');
+  if ((iterNum === 0 && Number(res) === number1 + number2) ||
+    (iterNum === 1 && Number(res) === number1 - number2) ||
+    (iterNum === 2 && Number(res) === number1 * number2)) {
+    console.log('Correct!');
+    return 0;
+  }
+
+  console.log(`${res} is wrong answer`);
+  if (iterNum === 0) {
+    console.log(`The right one is ${number1 + number2}`);
+  } else if (iterNum === 1) {
+    console.log(`The right one is ${number1 - number2}`);
+  } else if (iterNum === 2) {
+    console.log(`The right one is ${number1 * number2}`);
+  }
+  console.log(`Let's try again, ${name}`);
   return 1;
 };
 
