cf28f43b936e8a42e3f21de932a90e997ed099d0
Goncharov Roman <romg92@mail.ru>
2017-11-03 23:23:54 +0300

    get less complexity

--- src/index.js
+++ src/index.js
@@ -8,20 +8,36 @@ const questionFunction = (task) => {
   return userName;
 };
 
-export const questionAnswer = (nums, rand) => {
+const questionAnswer = (nums, rand) => {
   const numOut = nums.slice(0, rand).join(' ');
   console.log(`Question: ${numOut}`);
   const res = readlineSync.question('Your answer: ');
   return res;
 };
 
-export const failOut = (wrongResult, trueResult, name) => {
+const failOut = (wrongResult, trueResult, name) => {
   console.log(`'${wrongResult}' is wrong answer ;(`);
   console.log(`The right one is ${trueResult}`);
   console.log(`Let's try again, ${name}`);
 };
 
-export const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+const randomNums = (min, max, rand, acc) => {
+  const nums = [];
+  nums[0] = getRandom(min, max);
+  nums[1] = getRandom(min, max);
+  if (rand > 2) {
+    nums[0] = getRandom(min, max);
+    if (acc === 0) {
+      nums[1] = '+';
+    } else {
+      nums[1] = acc === 1 ? '-' : '*';
+    }
+    nums[2] = getRandom(min, max);
+  }
+  return nums;
+};
 
 export const gameFunction = (task, func, min, max, rand) => {
   const userName = questionFunction(task);
@@ -30,18 +46,7 @@ export const gameFunction = (task, func, min, max, rand) => {
       console.log(`Congratulations, ${userName}!`);
       return 'win';
     }
-    const numbers = [];
-    numbers[0] = getRandom(min, max);
-    numbers[1] = getRandom(min, max);
-    if (rand > 2) {
-      numbers[0] = getRandom(min, max);
-      if (acc === 0) {
-        numbers[1] = '+';
-      } else {
-        numbers[1] = acc === 1 ? '-' : '*';
-      }
-      numbers[2] = getRandom(min, max);
-    }
+    const numbers = randomNums(min, max, rand, acc);
     const res = questionAnswer(numbers, rand);
     const result = func(numbers, res, acc);
     if (result === 'yes') {
